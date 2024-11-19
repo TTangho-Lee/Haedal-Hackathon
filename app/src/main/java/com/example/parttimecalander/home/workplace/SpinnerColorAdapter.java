13921578b95ed1b05f9378410f484c4304de92b7
yeoby97 <hydudduq@naver.com>
2024-11-19 17:02:13 +0900

    ㄱㄱ

--- app/src/main/java/com/example/parttimecalander/home/workplace/SpinnerColorAdapter.java
+++ app/src/main/java/com/example/parttimecalander/home/workplace/SpinnerColorAdapter.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+import com.example.parttimecalander.R;
+
 import java.util.List;
 
 public class SpinnerColorAdapter extends BaseAdapter {
