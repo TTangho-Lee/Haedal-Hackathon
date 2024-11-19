cc8e80ee1892c1dca22ddd6cc9c9476ef1728fbe
yeoby97 <hydudduq@naver.com>
2024-11-19 21:15:22 +0900

    수정 완료

--- app/src/main/java/com/example/parttimecalander/home/workplace/WorkPlaceRegisterActivity.java
+++ app/src/main/java/com/example/parttimecalander/home/workplace/WorkPlaceRegisterActivity.java
@@ -67,6 +67,8 @@ public class WorkPlaceRegisterActivity extends AppCompatActivity {
         dailyDatabase = WorkDailyDatabase.getDatabase(this);
         dailyDao = dailyDatabase.workDailyDao();
 
+
+        
         // setupDay 호출
         setupDay(binding.checkboxMonday, binding.timeMonday, binding.startTimeMonday, binding.endTimeMonday);
         setupDay(binding.checkboxTuesday, binding.timeTuesday, binding.startTimeTuesday, binding.endTimeTuesday);
@@ -229,6 +231,9 @@ public class WorkPlaceRegisterActivity extends AppCompatActivity {
                             endTimeList.add(date + " " + endTimes[i]);
                         }
                     }
+
+                    // 다음 날로 이동
+                    calendar.add(Calendar.DAY_OF_MONTH, 1);
                 }
                 for (String startTime : startTimeList) {
                     System.out.println(startTime);
