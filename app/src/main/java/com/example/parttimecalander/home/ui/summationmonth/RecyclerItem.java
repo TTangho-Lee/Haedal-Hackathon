aaf34fb07c496de8ab7a2e422735d9aefd591fbd
SeungHo <seungho4136@naver.com>
2024-11-23 00:29:06 +0900

    주별요약 임시

--- app/src/main/java/com/example/parttimecalander/home/ui/summationmonth/RecyclerItem.java
+++ app/src/main/java/com/example/parttimecalander/home/ui/summationmonth/RecyclerItem.java
@@ -4,13 +4,17 @@ import java.io.Serializable;
 
 
 public class RecyclerItem implements Serializable{
+    public int year;
+    public int month;
     public String name;
     public int[][] worked_time;
     public boolean juhyu;
     public int pay;
     public String ColorHex;
 
-    public RecyclerItem(String name,int[][] worked_time,boolean juhyu,int pay,String ColorHex) {
+    public RecyclerItem(int year,int month,String name,int[][] worked_time,boolean juhyu,int pay,String ColorHex) {
+        this.year=year;
+        this.month=month;
         this.name=name;
         this.worked_time=worked_time;
         this.juhyu=juhyu;
